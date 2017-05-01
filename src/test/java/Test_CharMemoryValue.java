bacde97eb2f5d3e61064250a86119470a3ff98eb
quguocheng <quguocheng@meituan.com>
2017-05-01 18:01:58 +0800

    Java中，一个char、一个String，在内存中是如何存储的呢？

---
+++ src/test/java/Test_CharMemoryValue.java
@@ -0,0 +1,38 @@
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 在程序运行时刻,char/String等字符串在内存中是如何存储的呢?
+ *
+ */
+public class Test_CharMemoryValue {
+    /*
+     * char是primitive类型,应该是直接存储其数值,从0x0000到0xFFFF,这个数值直接对应Unicode Code Point吗?
+     * 请看下面的例子
+     */
+    @Test
+    public void unicodeText_charArray(){
+        char[] charArray=new char[]{
+                (char)0x6211,
+                (char)0x7684,
+                (char)0x540D,
+                (char)0x5B57,
+                (char)0x662F,
+        };
+        String textFromCharArray=new String(charArray);
+        System.out.println(textFromCharArray);
+        String unicodeText="\u6211\u7684\u540D\u5B57\u662F";
+        System.out.println(unicodeText);
+        Assert.assertEquals(textFromCharArray,unicodeText);
+    }
+    @Test
+    public void escapeJava(){
+        //下面这一行代码定义了一个字符串,那么Java程序应该是将每一个字符都存储为一个char类型(数值).
+        String originalStr="我的名字是andy qu\t!";
+        String escapedStr = StringEscapeUtils.escapeJava(originalStr);
+        System.out.println(escapedStr);
+        String deescapedStr=StringEscapeUtils.unescapeJava(escapedStr);
+        System.out.println(deescapedStr);
+    }
+}
